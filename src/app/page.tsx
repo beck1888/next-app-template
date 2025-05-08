f3ca0c8643d16d4c41c232d9793c998244ec2dfe
Beck Orion <beckorion@gmail.com>
2025-05-07 20:43:06 -0700

    added comment for quickstart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
+    // When you're ready to build, replace the code with <div></div>
     <div className="min-h-screen flex items-center justify-center flex-col gap-4">
       <Image src="/images/next-logo.png" alt="Next.js logo" width={400} height={400} />
       <h2 className="text-xl text-gray-600">See the readme to get started.</h2>
