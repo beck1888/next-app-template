bbc6343c439c8ff80e8b3ab11365fecb8864efa7
Beck Orion <beckorion@gmail.com>
2025-05-07 20:40:28 -0700

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Next.js Template App</h1>
+    <div className="min-h-screen flex items-center justify-center flex-col gap-4">
+      <Image src="/images/next-logo.png" alt="Next.js logo" width={400} height={400} />
+      <h2 className="text-xl text-gray-600">See the readme to get started.</h2>
     </div>
   );
 }
